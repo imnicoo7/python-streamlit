27668b74adca5e603d2183a74332fc8a7a6e85f5
Nicolas Gutierrez <nicolasgutierrez029@gmail.com>
2022-12-16 17:54:46 -0500

    app terminada con streamlit

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time 
-st.title('Uber pickups in NYC')
+st.title('Recogidas en uber en la gran ciudad de new yorkkkkkkk :)')
+
+
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -17,34 +19,32 @@ def load_data(nrows):
     return data
 
 # Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
+data_load_state = st.text('Cargando la info, espera...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
 # Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+data_load_state.text('Data cargada... excelente!')
 
-st.subheader('Raw data')
+st.subheader('Datos sin procesar')
 st.write(data)
 
-st.subheader('Number of pickups by hour')
+st.subheader('Cantadidad de recogidas por hora')
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 
 st.bar_chart(hist_values)
 
+st.write(' se puede ver que en el horario que más las personas piden uber es a ls 17 con casi 800 recogidas :o')
 
-st.subheader('Map of all pickups')
 
-st.map(data)
-
-hour_to_filter = st.slider('buscar un horario', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+hour_to_filter = st.slider('puedes ver un horario de las 0 a 23 horas', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
 
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.subheader(f'Mapa de todas las recogidas a las: {hour_to_filter}:00')
 st.map(filtered_data)
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
+if st.checkbox('Mostrar datos sin procesar  '):
+    st.subheader('Datos sin procesar')
     st.write(data)
 
 st.write('Nicolas Gutierrez')
@@ -52,21 +52,14 @@ st.write('Nicolas Gutierrez')
 
 
 
+st.write("Aquí está nuestro primer intento de usar datos para crear una tabla:")
 
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'animes buenos': ['Dragon ball z', 'Bokuno hero', 'no me se más xd'],
+  'calificación': [10, 9, 0]
 })
 
-df
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
-
-dataframe = np.random.randn(10, 10)
-st.dataframe(dataframe)
+st.dataframe(df)
 
 latest_iteration = st.empty()
 bar = st.progress(0)
